cb2da30dbfa5ee2ebc0277295a4aa39dafc303a8
taps210 <114672065+taps210@users.noreply.github.com>
2024-09-08 22:59:35 +0800

    Finalized header

--- CSOPESY/CSOPESY.cpp
+++ CSOPESY/CSOPESY.cpp
@@ -5,18 +5,23 @@
 using namespace std;
 
 void printHeader() {
-    cout << "  CCCC   SSSSS    OOOO    PPPPP    EEEEE   SSSSS   Y   Y" << endl;
-    cout << " C       S       O    O   P    P   E       S        Y Y " << endl;
-    cout << " C       SSSSS   O    O   PPPPP    EEEEE   SSSSS     Y  " << endl;
-    cout << " C           S   O    O   P        E           S     Y   " << endl;
-    cout << "  CCCC   SSSSS    OOOO    P        EEEEE   SSSSS     Y  " << endl;
+    cout << "  CCCC   SSSSS    OOOO    PPPPP    EEEEE   SSSSS   Y   Y\n";
+    cout << " C       S       O    O   P    P   E       S        Y Y \n";
+    cout << " C       SSSSS   O    O   PPPPP    EEEEE   SSSSS     Y  \n";
+    cout << " C           S   O    O   P        E           S     Y   \n";
+    cout << "  CCCC   SSSSS    OOOO    P        EEEEE   SSSSS     Y  \n";
+
+    cout << "\033[32m";
+    cout << "Hello, Welcome to CSOPESY commandline!\n";
+    cout << "\033[33m";
+    cout << "Type 'exit' to quit, 'clear' to clear the screen\n";
+    cout << "\033[0m";
 }
 
 int main()
 {
     printHeader();
-
-    cout << "\nHello World!\n";
+    cout << "Enter a command: \n";
     return 0;
 }
 
